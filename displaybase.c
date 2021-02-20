b1d270def6b2bd929845ca413761fac99c0c4882
Peter Bieringer <pb@bieringer.de>
2021-02-20 22:45:55 +0100

    scale "Text Vertical Offset" according to reduced size

--- displaybase.c
+++ displaybase.c
@@ -422,7 +422,11 @@ void cDisplay::DrawChar(int x, int y, cTeletextChar c) {
             cBitmap charBm(w, h, 24);
             charBm.DrawRectangle(0, 0, w, h, bg);
 //            charBm.DrawText(0, 0, buf, fg, bg, font);
+#ifdef OSDSIZEPCT
+            charBm.DrawText(0, (ttSetup.txtVoffset * ttSetup.OSDheightPct) / 100, buf, fg, 0, font);
+#else
             charBm.DrawText(0, ttSetup.txtVoffset, buf, fg, 0, font);
+#endif
             osd->DrawBitmap(vx, vy, charBm);
 #endif
         }
